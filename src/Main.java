791009b2256768c57c4c7245f68308fe15d9d999
Pierre-123 <pierre_genric_cabinbin@dlsu.edu.ph>
2024-04-04 09:31:33 +0800

    Team Formation 1.0
    It works! I think? I don't know how to explain it other than I'm just trying to form teams one at a time and thats how I'm facilitating the team formation.

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,9 @@ public class Main {
     private static ExecutorService executor;
     private static int regularCitizensRemaining;
     private static int superCitizensRemaining;
+    private static boolean shutdownRequested = false;
+    private static int n_rC=0;
+    private static int n_sC=0;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -48,17 +51,27 @@ public class Main {
     }
 
     public synchronized void teamUp(String type, int id) {
-        int n_rC=0;
-        int n_sC=0;
         try {
+            if (shutdownRequested) return;
             if(type.equals("Regular")){
                 if(regularCitizenSemaphore.tryAcquire()){
                     regularCitizensRemaining--;
                     n_rC++;
                     cur_team.acquire();
                     System.out.println("Regular joined up");
+                    System.out.println("n_rC: " + n_rC);
                 } else {
-                    System.out.println("Regular failed to join");
+                    System.out.println("Regular is waiting for Super");
+                    Thread.sleep(1000); // Wait for Super citizens to join
+                    if (superCitizenSemaphore.tryAcquire()) {
+                        superCitizensRemaining--;
+                        n_sC++;
+                        cur_team.acquire();
+                        System.out.println("Super joined up after Regular's request");
+                    } else {
+                        System.out.println("Super failed to join even after waiting");
+                        return; // Can't form a team, return
+                    }
                 }
             } else {
                 if(superCitizenSemaphore.tryAcquire()){
@@ -66,8 +79,19 @@ public class Main {
                     n_sC++;
                     cur_team.acquire();
                     System.out.println("Super joined up");
+                    System.out.println("n_sC: " + n_sC);
                 } else {
-                    System.out.println("Super failed to join");
+                    System.out.println("Super is waiting for Regular");
+                    Thread.sleep(1000); // Wait for Regular citizens to join
+                    if (regularCitizenSemaphore.tryAcquire()) {
+                        regularCitizensRemaining--;
+                        n_rC++;
+                        cur_team.acquire();
+                        System.out.println("Regular joined up after Super's request");
+                    } else {
+                        System.out.println("Regular failed to join even after waiting");
+                        return; // Can't form a team, return
+                    }
                 }
             }
             System.out.println("Current team: " + teamsSent);
@@ -75,24 +99,31 @@ public class Main {
 
 
             if(cur_team.availablePermits()==0){
-                for(;n_rC >= 0; n_rC--){
-                    regularCitizenSemaphore.release();
-                }
-                for(;n_sC >= 0; n_sC--){
-                    superCitizenSemaphore.release();
-                }
+                regularCitizenSemaphore.release(n_rC);
+                superCitizenSemaphore.release(n_sC);
                 cur_team.release(4);
+                System.out.println("Regular Citizen Permits: " + regularCitizenSemaphore.availablePermits());
+                System.out.println("Super Citizen Permits: " + superCitizenSemaphore.availablePermits());
+                System.out.println("Current Team Reset:" + cur_team.availablePermits());
                 System.out.println("Team " + teamsSent + " going out.");
                 teamsSent++;
+                n_rC=0;
+                n_sC=0;
+                System.out.println("n_rC: " + n_rC + " n_sC: " + n_sC);
             }
-
-            if(superCitizensRemaining == 0 && regularCitizensRemaining > 0 || (superCitizensRemaining+regularCitizensRemaining) < 4) {
+            //if regular citizens is in between 0-3 (assuming no more supers) or if no more supers and reg citizens is over 4 and cur_team is like not lacking in reg citizens
+            if(regularCitizensRemaining >= 0 && regularCitizensRemaining < 4 || superCitizensRemaining == 0 && regularCitizensRemaining > 4 && cur_team.availablePermits() > 2) {
+                System.out.println(cur_team.availablePermits());
                 System.out.println("Remaining Regular Citizens went back home: " + regularCitizensRemaining);
                 System.out.println("Remaining Super Citizen went back home: " + superCitizensRemaining);
                 System.out.println("Total Teams sent: " + teamsSent);
-                executor.shutdown();
+                synchronized (Main.class) {
+                    if (!shutdownRequested) {
+                        executor.shutdown();
+                        shutdownRequested = true;
+                    }
+                }
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
